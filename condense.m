89409b09bc79a2909a1d58e39151248f4011c97c
Alex <agonopol@gmail.com>
2017-01-29 16:16:45 +0100

    [ag] tsne movies

--- condense.m
+++ condense.m
@@ -8,29 +8,30 @@ addpath('./lib');
 % diffuse
 
 % load the data, clean transform
-path = fullfile(pwd(), 'data/6170_D1_splorm_0_normalized_Clean.fcs');
+path = fullfile(pwd(), 'data/6300_HF_D1_splorm_0_normalized_Clean.fcs');
 obj = CyTOFData(path);
 obj.dataTransformed = CyTOFData.transform(obj.data, 1);
 markers = obj.markerNames';
 channels = find(arrayfun(@(x1) ischar(x1{1}) && ~isempty(strfind(x1{1}, '_')) && isempty(strfind(x1{1}, 'DNA')) , markers));
 data = obj.dataTransformed(:, channels');
 
-v = VideoWriter('results/condense/6170_D1.avi');
+v = VideoWriter('results/condense/6300_HF_D1_tsne.avi');
 v.FrameRate = 15;
-open(v)
+open(v);
 
-scatterX(data);
-xlim([-12,12]);
-ylim([-12,12]);
+scatterX(data, 'dimensionalityReductionMethod', 'tsne');
+% xlim([-12,12]);
+% ylim([-12,12]);
 writeVideo(v,getframe(gcf));
     
-N=20;
+N=5;
 for i = 1:N
     data = condenseStep(data);
-    scatterX(data);
-    xlim([-12,12]);
-    ylim([-12,12]);
+    scatterX(data, 'dimensionalityReductionMethod', 'tsne');
+%     xlim([-12,12]);
+%     ylim([-12,12]);
     writeVideo(v,getframe(gcf));
 end
 
-close(v)
+close(v);
+close all;
