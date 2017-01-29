58141b35a57b00a0849193b60d1cb8e8d6f7f03c
Alex <agonopol@gmail.com>
2017-01-29 14:19:43 +0100

    [ag] added movies for simple condense step

--- condense.m
+++ condense.m
@@ -8,15 +8,29 @@ addpath('./lib');
 % diffuse
 
 % load the data, clean transform
-path = fullfile(pwd(), 'data/6300_Blood_D1_splorm_0_normalized_Clean.fcs');
+path = fullfile(pwd(), 'data/6170_D1_splorm_0_normalized_Clean.fcs');
 obj = CyTOFData(path);
 obj.dataTransformed = CyTOFData.transform(obj.data, 1);
 markers = obj.markerNames';
 channels = find(arrayfun(@(x1) ischar(x1{1}) && ~isempty(strfind(x1{1}, '_')) && isempty(strfind(x1{1}, 'DNA')) , markers));
 data = obj.dataTransformed(:, channels');
 
-% generate distance matrix
-[distanceMatrix, indicatorMatrix] = calcDistanceMatrix(data);
-[normalizedAffinityMatrix, Q] = calcNormalizedAffinityMatrix(distanceMatrix, indicatorMatrix);
-diffusedNormalizedAffinityMatrix = diffuse(normalizedAffinityMatrix, Q);
-% C = louvain(full(diffusedNormalizedAffinityMatrix));
+v = VideoWriter('results/condense/6170_D1.avi');
+v.FrameRate = 15;
+open(v)
+
+scatterX(data);
+xlim([-12,12]);
+ylim([-12,12]);
+writeVideo(v,getframe(gcf));
+    
+N=20;
+for i = 1:N
+    data = condenseStep(data);
+    scatterX(data);
+    xlim([-12,12]);
+    ylim([-12,12]);
+    writeVideo(v,getframe(gcf));
+end
+
+close(v)
