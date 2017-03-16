12116d3e47d4f859dda75e55043dc56a0d72065b
Alex <agonopol@gmail.com>
2017-03-16 21:51:46 +0100

    [ag] added single cell

--- contract_single_cell.m
+++ contract_single_cell.m
@@ -13,12 +13,18 @@ options.fastStop = false;
 
 path = 'library_size_normalized_scRNA-seq_DB.mat';
 data = load(path);
+
+
     
 [~, name, ~] = fileparts(path);
 options.destination = fullfile(pwd(), 'results', 'contract', name, 'DB1_umi50_norm', '//');
 [dest, ~, ~] = fileparts(options.destination);
 mkdir_if_not_exists(dest);
  
+%remove empty rows / columsn 
+data.DB1_umi50_norm( ~any(data.DB1_umi50_norm,2), : ) = [];  %rows
+data.DB1_umi50_norm( :, ~any(data.DB1_umi50_norm,1) ) = [];  %columns
+
 contractor = ContractionClustering(data.DB1_umi50_norm, [], options);
 contractor = contractor.contract();
 clc;
